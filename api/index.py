630c2163a566c0c724875a2f12bcdd891d6cb45d
Jonas <agencialobuzlancamentos@gmail.com>
2026-03-21 01:13:48 -0300

    feat: migrate to stlite for vercel deployment

--- api/index.py
+++
@@ -1,8 +0,0 @@
-from app.main import main
-
-# Ponto de entrada padrão para a Vercel reconhecer o projeto Python
-# O Streamlit é um servidor, mas ter este arquivo na pasta api/ 
-# resolve o erro de 'No python entrypoint found'.
-
-if __name__ == "__main__":
-    main()
